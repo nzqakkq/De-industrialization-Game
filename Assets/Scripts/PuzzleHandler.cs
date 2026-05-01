1e3916416a4305a68b26ddcdfcbfcbec48d47538
nzqakkq <taimullah30@gmail.com>
2026-05-02 02:38:46 +0400

    Dam Puzzle Completion Logic

--- Assets/Scripts/PuzzleHandler.cs
+++ Assets/Scripts/PuzzleHandler.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PuzzleHandler : MonoBehaviour
 {
@@ -93,6 +94,7 @@ public class PuzzleHandler : MonoBehaviour
         mouseScript.enabled = true;
         Cursor.lockState = CursorLockMode.Locked;
         puzzleCanvas.SetActive(false);
+        SceneManager.LoadScene("Scenes/Puzzle Complete");
     }
 
     public void OnPauseClick(Button pauseButton)
